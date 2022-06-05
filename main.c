7b00dc45ac5b3ce25ef5047e6773bf21db9ff158
su4ovden <denskykin381@gmail.com>
2022-06-05 16:40:40 +0300

    Added a mock-up of the implementation of communication with the sensor

--- main.c
+++ main.c
@@ -2,11 +2,28 @@
  * Ventilation.c
  *
  * Created: 31.05.2022 19:59:54
- * Author : Denis
+ * Author : su4ovden
  */ 
 
 #include <avr/io.h>
 #include <util/delay.h>
+#include "twi.h"
+
+#define SENSOR_ADDRESS 0xB8
+
+typedef enum
+{
+	READING_REGISTER_DATA = 0x03,
+	WRITE_MULTIPLE_REGISTERS = 0x10
+} function_code_t; 
+
+typedef struct 
+{
+	int16_t temperature;
+	uint16_t humidity;
+} sensor_data_t;
+
+sensor_data_t sensor_data;
 
 void portb_init(void)
 {
@@ -28,11 +45,69 @@ void timer2_init(void)
 	OCR2 = 10;
 }
 
+void sensor_wakeup(void)
+{
+	twi_master_action(TWI_START);
+	
+	twi_transmit_byte(SENSOR_ADDRESS | W);
+	twi_master_action(TWI_TRANSMIT);
+	
+	_delay_us(850);
+	
+	twi_master_action(TWI_STOP);
+}
+
+void sensor_write_data(uint8_t* data, uint8_t length)
+{
+	twi_master_action(TWI_START);
+	
+	twi_transmit_byte(SENSOR_ADDRESS | W);
+	twi_master_action(TWI_TRANSMIT);
+	
+	for(uint8_t i = 0; i < length; i++)
+	{
+		twi_transmit_byte(data[i]);
+		twi_master_action(TWI_TRANSMIT);
+	}
+	
+	twi_master_action(TWI_STOP);
+}
+
+void sensor_read_data(uint8_t* data, uint8_t length)
+{
+	twi_master_action(TWI_START);
+	twi_transmit_byte(SENSOR_ADDRESS | R);
+	
+	for(uint8_t i = 0; i < length - 1; i++)
+	{
+		data[i] = twi_recive_byte();
+		twi_master_action(TWI_RETURN_ACK);
+	}
+	
+	data[length - 1] = twi_recive_byte();
+	twi_master_action(TWI_RETURN_NACK);
+	
+	twi_master_action(TWI_STOP);
+}
+
+void sensor_get_data(sensor_data_t* sensor_data, uint8_t start_address, uint8_t length)
+{
+	uint8_t sending_data_array[3] = {READING_REGISTER_DATA, start_address, length};
+	uint8_t recieved_data_buffer[length + 4]; // length - length of requested information, 4 - length of additional information
+		
+	sensor_write_data(sending_data_array, 3);
+	sensor_read_data(recieved_data_buffer, length + 4);
+	
+	sensor_data->humidity = (recieved_data_buffer[2] << 8) | recieved_data_buffer[3];
+	sensor_data->temperature = (recieved_data_buffer[4] << 8) | recieved_data_buffer[5];
+}
+
 int main(void)
 {
 	portb_init();
 	timer1_init();
 	timer2_init();
+	twi_master_mode_init(100000);
 	
     while (1) 
     {		
