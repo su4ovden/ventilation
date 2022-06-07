6216cc9104bfc85bb651866ba97629ef5ffd012a
su4ovden <denskykin381@gmail.com>
2022-06-07 22:40:20 +0300

    Fixed the formula for calculating the clock frequency

--- twi.c
+++ twi.c
@@ -7,25 +7,30 @@
 
 #include "twi.h"
 
-void twi_master_mode_init(uint32_t scl_frequence){
-	TWBR = ((F_CPU * 2 * (4^(TWSR & 0x03))) / scl_frequence) - 16;
+void twi_master_mode_init(uint32_t scl_frequence)
+{
+	TWBR = ((F_CPU / scl_frequence) - 16) / (2 * (1 << ((TWSR & 0x03) * 2)));
 	TWCR = (1<<TWEN);
 }
 
-void twi_slave_mode_init(uint8_t slave_address){
+void twi_slave_mode_init(uint8_t slave_address)
+{
 	TWAR = (slave_address & 0x7F) << 1;
 	TWCR = (1<<TWEA) | (1<<TWEN);
 }
 
-void twi_transmit_byte(uint8_t byte){
+void twi_transmit_byte(uint8_t byte)
+{
 	TWDR = byte;
 }
 
-uint8_t twi_recive_byte(void){
+uint8_t twi_recive_byte(void)
+{
 	return TWDR;
 }
 
-uint8_t twi_master_action(twi_action_t action){
+uint8_t twi_master_action(twi_action_t action)
+{
 	switch (action){
 		case TWI_START:
 		case TWI_REPEAT_START:
@@ -54,7 +59,8 @@ uint8_t twi_master_action(twi_action_t action){
 	return (TWSR & 0xF8);
 }
 
-uint8_t twi_slave_action(twi_action_t action){
+uint8_t twi_slave_action(twi_action_t action)
+{
 	switch (action){
 		case TWI_START:
 			TWCR = (1<<TWINT) | (1<<TWSTA) | (1<<TWEN);
