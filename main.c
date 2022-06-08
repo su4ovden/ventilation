b3b256db9d1b99414212d6b8de2cca38633737fd
su4ovden <denskykin381@gmail.com>
2022-06-08 19:33:27 +0300

    Added support for LEDs and implemented communication with the bluetooth module

--- main.c
+++ main.c
@@ -7,16 +7,45 @@
 
 #include <avr/io.h>
 #include <util/delay.h>
+#include <avr/interrupt.h>
 #include "twi.h"
 #include "am2320.h"
 #include "usart.h"
 
+#define LED_GREEN_ON (PORTC |= (1<<PINC0))
+#define LED_GREEN_OFF (PORTC &= ~(1<<PINC0))
+
+#define LED_YELLOW_ON (PORTC |= (1<<PINC1))
+#define LED_YELLOW_OFF (PORTC &= ~(1<<PINC1))
+
 sensor_data_t sensor_data;
+volatile uint8_t last_byte;
+
+ISR(USART_RXC_vect)
+{
+	last_byte = UDR;
+	if(last_byte == '1')
+	{
+		LED_GREEN_ON;
+		usart_transmit_string("ON\r\n");
+	}
+	else
+	{
+		LED_GREEN_OFF;
+		usart_transmit_string("OFF\r\n");
+	}
+}
 
 void portb_init(void)
 {
 	DDRB |= (1<<PINB3) | (1<<PINB2) | (1<<PINB1);
 	PORTB &= ~((1<<PINB3) | (1<<PINB2) | (1<<PINB1));
+}
+
+void portc_init(void)
+{
+	DDRC |= (1<<PINC1) | (1<<PINC0);
+	PORTC &= ~((1<<PINC1) | (1<<PINC0));
 }
 
 void timer1_init(void)
@@ -36,23 +65,27 @@ void timer2_init(void)
 int main(void)
 {
 	portb_init();
+	portc_init();
+	
+	for (uint8_t i = 0; i < 5; i++)
+	{
+		LED_GREEN_ON;
+		_delay_ms(50);
+		LED_GREEN_OFF;
+		_delay_ms(50);
+	}
+	
 	timer1_init();
 	timer2_init();
-	twi_master_mode_init(10000);
-	usart_init(DEFAULT_MODE, DEFAULT_FRAME_FORMAT, 57200);
+	twi_master_mode_init(100000);
+	usart_init(DEFAULT_MODE, DEFAULT_FRAME_FORMAT, 9600);
 	
-	sensor_wakeup();
-	_delay_ms(10);
-	sensor_get_environment_data(&sensor_data);
-	_delay_ms(2200);
+	SREG |= (1<<7);
 	
     while (1) 
     {	
-		usart_transmit_bytes((uint8_t*) &sensor_data, 2);
-		sensor_wakeup();
-		_delay_ms(10);
-		sensor_get_environment_data(&sensor_data);
-		_delay_ms(2200);
+		usart_transmit_string("Hello from firmware!\r\n");
+		_delay_ms(1000);
     }
 }
 
